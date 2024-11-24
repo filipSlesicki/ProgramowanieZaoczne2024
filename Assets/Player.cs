fc187eaddeb03850caf3c5b2d6fd127c85d6ec4f
filipSlesicki <>
2024-11-24 13:45:29 +0100

    Player movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,30 +4,54 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //Zmienne
-    // int - Liczba ca³kowita (1, 220, -234)
-    // float - liczba z przeinkiem (1.2f, -0.23f, 102934f)
-    // string - tekst ("jakiœ tekst")
-    // bool - zmienna logiczna (true,false)
-
-    // Mpodyfikator dostêpu Typ nazwa 
-    public float fallingSpeed = 1;
+    public float movementSpeed = 1;
     public float rotationSpeed = 1;
-
 
     // Komentarz
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // GetAxis : A = -1 , D = 1, Brak inputu = 0
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 moveVector = new Vector3(horizontalInput, verticalInput, 0); // kierunek
+        //if(moveVector.magnitude > 1 )
+        //{
+        //    // Normalize() ustawia d³ugoœæ wektora na 1, zachowuj¹c kierunek
+        //    moveVector.Normalize();
+        //}
+
+        moveVector = Vector3.ClampMagnitude(moveVector, 1); // Ogranicza d³ugoœæ wektora do podanej wartoœæi ( w tym wypadku 1)
+        moveVector *= Time.deltaTime * movementSpeed; // prêdkoœæ
+        transform.Translate(moveVector);
+        //transform.position += moveVector;
+        
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.Translate(movementSpeed * Time.deltaTime, 0, 0);
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.Translate(-movementSpeed * Time.deltaTime, 0, 0);
+        //}
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    transform.Translate(0, movementSpeed * Time.deltaTime, 0);
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.Translate(0, -movementSpeed * Time.deltaTime, 0);
+        //}
+
         // Mno¿ymy razy Time.deltaTime ¿eby zamieniæ prêdkoœæ na klatkê na prêdkoœæ na sekundê
         //transform.Translate(0,-1 * Time.deltaTime,0);
-        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        //transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
         //transform.position += Vector3.down * fallingSpeed * Time.deltaTime;
         // Inny zapis
         // tansform.position = transform.position + Vector3.down * Time.deltaTime;
--- Assets/Variables.cs
+++ Assets/Variables.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Zmienne
+// int - Liczba ca³kowita (1, 220, -234)
+// float - liczba z przeinkiem (1.2f, -0.23f, 102934f)
+// string - tekst ("jakiœ tekst")
+// bool - zmienna logiczna (true,false)
 public class Variables : MonoBehaviour
-{
+{    
+    // Mpodyfikator dostêpu Typ nazwa 
     public int b = 10;
     int health = 10;
     public int damage = 10;
@@ -50,6 +56,18 @@ public class Variables : MonoBehaviour
         bool isBetween2Planes = transform.position.y > 0 && transform.position.y < 5; // && - AND
         bool isNotBetween2Planes = transform.position.y  < 0 || transform.position.y > 5; // || - OR
         isNotBetween2Planes = !isBetween2Planes; // ! - odwrotnoœæ
+        if(isBetween2Planes)
+        {
+            Debug.Log("Is between 2 planes");
+        }
+        else if(transform.position.y < 0) // Is above plane 1
+        {
+            Debug.Log("Is below 0");
+        }
+        else
+        {
+            Debug.Log("Is not between 2 planes");
+        }
         //Debug.Log(isBetween2Planes);
         //health -= damage;
         //bool isAlive = health > 0;
